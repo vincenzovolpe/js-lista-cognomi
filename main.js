b2f21d2ce8d03cb30f1b1e3cc28d2f08ca8c4a6d
vincenzovolpe <vinci792010@gmail.com>
2019-11-18 16:48:43 +0100

    Aggiustata logica del  sort

--- main.js
+++ main.js
@@ -2,20 +2,20 @@
 var listacognomi = ['Rossi', 'Bianchi', 'Celentano', 'Morandi'];
 console.log(listacognomi);
 
-// Chiedo ilcognome all'utente
+// Chiedo il cognome all'utente
 var cognomeutente = prompt('Per favore inserisci il tuo cognome');
 console.log(cognomeutente);
 
 // Inserisco il cognome nella lista dei cognomi già presenti
-listacognomi.push(cognomeutente);
+listacognomi = listacognomi.concat(cognomeutente);
 console.log(listacognomi);
 
 // Ordiniamo la lista dei cognomi
-listacognomi.sort();
+listacognomi = listacognomi.sort(function(a,b){return a - b});
 console.log(listacognomi);
 
 // Troviamo la posizione del cognome fornito dall'utente e la stampiamo
-trovato = false
+trovato = false;
 for (i= 0; (i < listacognomi.length) && (!trovato); i++) {
     if (listacognomi[i] == cognomeutente) {
         var posizione = i + 1;
