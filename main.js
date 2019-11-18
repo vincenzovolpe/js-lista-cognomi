3506fa4069e0750a8b45c8e777f5b86fc8227056
vincenzovolpe <vinci792010@gmail.com>
2019-11-18 18:43:27 +0100

    aggiornamento posizione

--- main.js
+++ main.js
@@ -5,9 +5,8 @@ console.log(listacognomi);
 // Chiedo il cognome all'utente
 var cognomeutente = String(prompt('Per favore inserisci il tuo cognome'));
 
-// trasforma la prima in maiuscolo visto che il mio array ha le iniziali dell stringhe in maiuscolo
+// trasforma la prima in maiuscolo visto che il mio array ha le iniziali delle stringhe in maiuscolo
 var cognomeutente = cognomeutente.charAt(0).toUpperCase() + cognomeutente.slice(1);
-
 console.log(cognomeutente);
 
 // Inserisco il cognome nella lista dei cognomi già presenti
@@ -20,10 +19,9 @@ console.log(listacognomi);
 
 // Troviamo la posizione del cognome fornito dall'utente e la stampiamo
 trovato = false;
-for (i= 0; (i < listacognomi.length) && (!trovato); i++) {
-    if (listacognomi[i] == cognomeutente) {
-        var posizione = i + 1;
+for (var posizione = 0; (posizione < listacognomi.length) && (!trovato); posizione++) {
+    if (listacognomi[posizione] == cognomeutente) {
         trovato = true;
     }
 }
-console.log("Il cognome dell'utente si trova nella posizione numero " + posizione + " della lista");
+console.log("Il cognome dell'utente si trova nella posizione numero " + (posizione + 1) + " della lista");
